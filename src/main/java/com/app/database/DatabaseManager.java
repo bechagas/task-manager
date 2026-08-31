c123d0d2c9dd6177fbdf7d634d449e1dffb463dc
bechagas <bechagas.chagas@gmail.com>
2026-08-31 14:10:56 -0300

    feat(database): setup database manager with ormlite and sqlite

--- src/main/java/com/app/database/DatabaseManager.java
+++ src/main/java/com/app/database/DatabaseManager.java
@@ -1,5 +1,29 @@
 package com.app.database;
 
+import com.app.model.Project;
+import com.app.model.Task;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import java.sql.SQLException;
+
 public class DatabaseManager {
-    
+   private static final String URL = "jdbc:sqlite:task_manager.db";
+   private static ConnectionSource connectionSource;
+  
+   public static ConnectionSource getConnection() throws SQLException {
+       if (connectionSource == null) {
+           connectionSource = new JdbcConnectionSource(URL);
+          
+           TableUtils.createTableIfNotExists(connectionSource, Project.class);
+           TableUtils.createTableIfNotExists(connectionSource, Task.class);
+       }
+       return connectionSource;
+   }
+  
+   public static void close() throws SQLException {
+       if (connectionSource != null) {
+           connectionSource.close();
+       }
+   }
 }
